6b5ba817ca7cf0abe5eb060dc629066299db0742
Pranavbala Ganesh <pranavb.ganesh@gmail.com>
2022-08-21 08:46:24 +0530

    Basic Dashboard

--- src/pages/Mobile/CollectorInput.js
+++ src/pages/Mobile/CollectorInput.js
@@ -12,38 +12,65 @@ import {
   Card,
   InputGroup,
 } from "react-bootstrap";
+import { Routes } from "../../routes";
 import { Typography } from "@material-tailwind/react";
-import { Scales, CaretDown, Camera, List, Recycle } from "phosphor-react";
+import {
+  Scales,
+  CaretDown,
+  Camera,
+  List,
+  Recycle,
+  CarSimple,
+  House, User
+} from "phosphor-react";
 
 export default () => {
-  var fileInput = document.getElementById("weight-collector");
-  var fileDisplayArea = document.getElementById("fileDisplayArea");
+  const [image, setImage] = React.useState("");
+  const imageRef = React.useRef(null);
 
-  window.onload = function () {
-    var fileInput = document.getElementById("weight-collector");
-    var fileDisplayArea = document.getElementById("fileDisplayArea");
+  function useDisplayImage() {
+    const [result, setResult] = React.useState("");
 
-    fileInput.addEventListener("change", function (e) {
-      // Put the rest of the demo code here.
-      var file = fileInput.files[0];
-      var imagetype = /image.*/;
-      if (file.type.match(imagetype)) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-          fileDisplayArea.innerHTML = "";
-          var img = new Image();
-          img.src = reader.result;
-          fileDisplayArea.appendChild(img);
-        };
-        reader.readAsDataURL(file);
-      } else {
-        fileDisplayArea.innerHTML = "File not supported!";
-      }
-    });
-  };
+    function uploader(e) {
+      const imageFile = e.target.files[0];
+
+      const reader = new FileReader();
+      reader.addEventListener("load", (e) => {
+        setResult(e.target.result);
+      });
+
+      reader.readAsDataURL(imageFile);
+    }
+
+    return { result, uploader };
+  }
+
+  const { result, uploader } = useDisplayImage();
+
+  const [imageUrl, setImageUrl] = React.useState("");
+  const imageUrlref = React.useRef(null);
+
+  function somethingimage() {
+    const [display, setdisplay] = React.useState("");
+
+    function uploaderu(e) {
+      const imageUrlfile = e.target.files[0];
+
+      const reader = new FileReader();
+      reader.addEventListener("load", (e) => {
+        setdisplay(e.target.result);
+      });
+      reader.readAsDataURL(imageUrlfile);
+    }
+    return { display, uploaderu };
+  }
+  const { display, uploaderu } = somethingimage();
+
+  const { result1, uploader1 } = useDisplayImage();
 
   return (
-    <>
+   <>
+    
       <Navbar variant="light">
         <Container>
           <Button variant="outline-*">
@@ -122,42 +149,124 @@ export default () => {
                     Don't forget to Click a picture
                   </Form.Text>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group className="mb-3">
                   <Form.Label>Upload Picture</Form.Label>
                   <Stack direction="Vertical" gap={3}>
-                  <Button variant="outline-secondary">
-                    <label for="weight-collector">
-                      <Camera />
-                    </label>
-                  </Button>
+                    <Button variant="outline-secondary">
+                      <label for="weight-collector">
+                        <Camera />
+                      </label>
+                    </Button>
 
-                  <input
-                    type="file"
-                    id="weight-collector"
-                    name="weight-collector"
-                    accept="image/*"
-                    capture="environment"
-                    style={{ display: "none" }}
-                  ></input>
-                  <img src="weight-collector"  />
+                    <input
+                      type="file"
+                      id="weight-collector"
+                      name="weight-collector"
+                      accept="image/*"
+                      capture="environment"
+                      onChange={(e) => {
+                        setImage(e.target.files[0]);
+                        uploader(e);
+                      }}
+                      style={{ display: "none" }}
+                    ></input>
+                    {result && <img ref={imageRef} src={result} alt="" />}
                   </Stack>
+                  <Form.Text className="text-muted">
+                    Picture of the React hate machine
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Types of Plastic</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Click to Select</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Recycling Facility</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Click to Select</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Upload Pictures of Collected Waste</Form.Label>
+                  <Stack direction="Vertical" gap={3}>
+                    <Button variant="outline-secondary">
+                      <label for="weight-collector">
+                        <Camera />
+                      </label>
+                    </Button>
+
+                    <input
+                      type="file"
+                      id="weight-collector1"
+                      name="weight-collector"
+                      accept="image/*"
+                      capture="environment"
+                      onChange={(e) => {
+                        setImageUrl(e.target.files[0]);
+                        uploader();
+                      }}
+                      style={{ display: "none" }}
+                    ></input>
+                    {display && <img ref={imageUrlref} src={display} alt="" />}
+                  </Stack>
+                  <Form.Text className="text-muted">
+                    Picture of the React hate machine
+                  </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                <Form.Group className="mb-3" controlId="weight-collected">
+                  <Form.Label>Vehicle No.</Form.Label>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">
+                      <CarSimple size={20} />
+                    </InputGroup.Text>
+                    <Form.Control
+                      placeholder="KAXX 00 XXXX"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                    />
+                  </InputGroup>
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Check me out" />
+                <Form.Group className="mb-3" controlId="weight-collected">
+                <div className="d-grid gap-2">
+                  <Button variant="primary" type="submit">
+                    {" "}
+                    Submit
+                  </Button>
+                </div>
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
+                <Container>
+                <div className="d-grid gap-2">
+                  <Button variant="primary" type="submit">
+                    {" "}
+                    Submit
+                  </Button>
+                 
+                </div>
+                </Container>
               </Form>
             </Card.Body>
           </Card>
+
         </Container>
       </Stack>
+      <Navbar fixed="bottom" className="bg-light">
+        <Container>
+          
+          <Button variant="outline-*"  size="sm"><List  size={32}/></Button>
+          <Button variant="outline-*" size="sm"><House size={32}/></Button>
+          <Button variant="outline-*" size="sm"><User size={32}/></Button>
+          
+        </Container>
+      </Navbar>
     </>
   );
 };
--- src/pages/Mobile/RecyclerConfirmation.js
+++ src/pages/Mobile/RecyclerConfirmation.js
@@ -1,205 +1,222 @@
 import React, { useRef, useEffect, useState } from "react";
-
-import Webcam from "react-webcam";
-import { Link } from "react-router-dom";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-
 import {
-    Col,
-    Row,
-    Card,
-    Button,
-    FormCheck,
-    Container,
+  Navbar,
+  Container,
+  Button,
+  Image,
+  Row,
+  Col,
+  Stack,
+  Figure,
+  Form,
+  Card,
+  InputGroup,
 } from "react-bootstrap";
-
 import { Routes } from "../../routes";
-import BgImage from "../../assets/img/illustrations/signin.svg";
-
+import { Typography, Color } from "@material-tailwind/react";
 import {
-    faCameraRetro,
-    faWeightHanging,
-    faAngleLeft,
-    faEnvelope,
-    faUnlockAlt,
-} from "@fortawesome/free-solid-svg-icons";
+  Scales,
+  CaretDown,
+  Camera,
+  List,
+  Recycle,
+  CarSimple,
+  House,
+  User,
+  Plus,
+  MapPinLine,
+  Clock
+} from "phosphor-react";
+import { Link } from "react-floating-action-button";
 
 export default () => {
-    var fileInput = document.getElementById('weight-collector');
-    var fileDisplayArea = document.getElementById('fileDisplayArea');
+  const [image, setImage] = React.useState("");
+  const imageRef = React.useRef(null);
 
-    window.onload = function () {
-        var fileInput = document.getElementById('weight-collector');
-        var fileDisplayArea = document.getElementById('fileDisplayArea');
+  function useDisplayImage() {
+    const [result, setResult] = React.useState("");
 
-        fileInput.addEventListener('change', function (e) {
-            var file = fileInput.files[0];
-            var imagetype = /image.*/;
-            if (file.type.match(imagetype)) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    fileDisplayArea.innerHTML = "";
-                    var img = new Image();
-                    img.src = reader.result;
-                    fileDisplayArea.appendChild(img);
-                }
-                reader.readAsDataURL(file);
-            } else {
-                fileDisplayArea.innerHTML = "File not supported!";
-            }
-        });
+    function uploader(e) {
+      const imageFile = e.target.files[0];
+
+      const reader = new FileReader();
+      reader.addEventListener("load", (e) => {
+        setResult(e.target.result);
+      });
+
+      reader.readAsDataURL(imageFile);
     }
 
+    return { result, uploader };
+  }
 
-    return (
-        <main>
-            <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
-                <Container>
-                    <p className="text-center">
-                        <Card.Link
-                            as={Link}
-                            to={Routes.DashboardOverview.path}
-                            className="text-gray-700"
-                        >
-                            Back to
-                            homepage
-                        </Card.Link>
-                    </p>
-                    <Row
-                        className="justify-content-center form-bg-image"
-                        style={{ backgroundImage: `url(${BgImage})` }}
-                    >
-                        <Col
-                            xs={12}
-                            className="d-flex align-items-center justify-content-center"
-                        >
-                            <div className="mb-4 mb-lg-0 bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
-                                <div className="text-center text-md-center mb-4 mt-md-0">
-                                    {/* Header */}
-                                    <h3 className="mb-0">Recycler Confirmation</h3>
-                                </div>
-                                <Form className="mt-4">
-                                    <div className="mt-3 mb-4 text-center"></div>
-                                    {/* Weight */}
-                                    <Form.Group id="recyclingfac" className="mb-4">
-                                        <Form.Label>Recycling Facility</Form.Label>
-                                        <InputGroup>
-                                            <DropdownButton
-                                                variant="outline-secondary"
-                                                title="Dropdown"
-                                                id="input-group-dropdown-2"
-                                                align="end">
-                                                <Dropdown.Item href="#">Facility 1</Dropdown.Item>
-                                                <Dropdown.Item href="#">Facility 2</Dropdown.Item>
-                                                <Dropdown.Item href="#">Facility 3</Dropdown.Item>
-                                            </DropdownButton>
-                                            <Form.Control aria-label="Text input with dropdown button" />
-                                        </InputGroup>
-                                    </Form.Group>
-                                    <Form.Group id="weight" className="mb-4">
-                                        <Form.Label>Weight of Waste Received (in Kgs)</Form.Label>
-                                        <InputGroup>
-                                            <InputGroup.Text>
-                                            
-                                            </InputGroup.Text>
+  const { result, uploader } = useDisplayImage();
 
-                                            <Form.Control
-                                                autoFocus
-                                                required
-                                                type="weight"
-                                                placeholder="100"
-                                            />
-                                        </InputGroup>
-                                        <div class="ml-5">
-                                            <Button>
-                                                <input
-                                                    type="file"
-                                                    id="weight-collector"
-                                                    name="weight-collector"
-                                                    accept="image/*"
-                                                    capture="environment"
-                                                ></input>
+  const [imageUrl, setImageUrl] = React.useState("");
+  const imageUrlref = React.useRef(null);
 
-                                     
-                                            </Button>
-                                        </div>
-                                        {/* Camera */}
-                                    </Form.Group>
-                                    <Form.Group id="processingMethod" className="mb-4">
-                                        <Form.Label>Processing Method</Form.Label>
-                                        <InputGroup>
-                                            <DropdownButton
-                                                variant="outline-secondary"
-                                                title="Dropdown"
-                                                id="input-group-dropdown-2"
-                                                align="end"
-                                            >
-                                                <Dropdown.Item href="#">Method 1</Dropdown.Item>
-                                                <Dropdown.Item href="#">Method 2</Dropdown.Item>
-                                                <Dropdown.Item href="#">Method 3</Dropdown.Item>
-                                            </DropdownButton>
-                                            <Form.Control aria-label="Text input with dropdown button" />
-                                        </InputGroup>
-                                    </Form.Group>
+  function somethingimage() {
+    const [display, setdisplay] = React.useState("");
 
-                                    <Form.Group id="imageWeight" className="mb-4">
-                                        <Form.Label>Images of Weight Received</Form.Label>
-                                        <div class="ml-5">
-                                            <Button>
-                                                <input
-                                                    type="file"
-                                                    id="weight-collector"
-                                                    name="weight-collector"
-                                                    accept="image/*"
-                                                    capture="environment"
-                                                ></input>
+    function uploaderu(e) {
+      const imageUrlfile = e.target.files[0];
 
-                                          
-                                            </Button>
-                                        </div>
-                                        {/* Camera */}
-                                    </Form.Group>
-                                    <Form.Group id="vehicle" className="mb-4">
-                                        <Form.Label>Vehicle Number</Form.Label>
+      const reader = new FileReader();
+      reader.addEventListener("load", (e) => {
+        setdisplay(e.target.result);
+      });
+      reader.readAsDataURL(imageUrlfile);
+    }
+    return { display, uploaderu };
+  }
+  const { display, uploaderu } = somethingimage();
 
-                                        <InputGroup>
+  const { result1, uploader1 } = useDisplayImage();
 
-                                            <Form.Control
-                                                autoFocus
-                                                required
-                                                placeholder="KAXX 00 1234"
-                                            />
-                                        </InputGroup>
+  return (
+    <>
+      <Navbar variant="light">
+        <Container>
+          <Button variant="outline-*">
+            <List size={24} />
+          </Button>
+        </Container>
+        <Container>
+          <Button variant="info" size="sm">
+            Contact Us
+          </Button>
+          <Button variant="outline-*" size="sm">
+            <img
+              src="https://picsum.photos/40/40"
+              width="20"
+              height="20"
+              class="rounded-circle"
+            ></img>
+          </Button>
+        </Container>
+      </Navbar>
+      <Stack gap={3}>
+        <Container className="mb-3">
+          <Row>
+            <Typography variant="h1">Good Morning John!</Typography>
 
+            <Stack direction="horizontal" className="col-md-5 mx-auto">
+              <Container>
+                {" "}
+                <Typography variant="lead"> Today's</Typography>
+                <Typography variant="h6">Weight</Typography>
+              </Container>
+              <div className="vr" style={{ color: "white" }} />
+              <Container>
+                {" "}
+                <Typography variant="lead"> Date's</Typography>
+                <Typography variant="h6">15/07/14</Typography>
+              </Container>
+              <div className="vr" style={{ color: "white" }} />
+              <Container>
+                {" "}
+                <Typography variant="lead">Time</Typography>
+                <Typography variant="h6">10:22</Typography>
+              </Container>
+            </Stack>
+          </Row>
+        </Container>
+      </Stack>
+      <Container className="mb-3">
+        <div className="d-grid gap-2">
+          <Button>
+            <Col>
+              <Plus size={20} /> <span>{"   "}</span>
+              Start a CGP cycle
+            </Col>
+          </Button>
+        </div>
+      </Container>
 
-                                        <Button>
-                                            <input
-                                                type="file"
-                                                id="weight-collector"
-                                                name="weight-collector"
-                                                accept="image/*"
-                                                capture="environment"
-                                            ></input>
+      <Stack>
+        <Container className="mb-3">
+          <Typography variant="h5"> Cycles in Progress</Typography>
+          <Stack gap={3}>
+            <Card>
+              <Card.Body>
+              
+              <div className="d-grid gap-2">
+                    <Recycle/><Typography> 472103847283</Typography>
+                   <Typography variant ="h4"> Plastic Type</Typography>
+                   <Typography variant ="h5"> Arriving at <MapPinLine/> <Typography variant="h5"> Station</Typography></Typography>
+                   <Typography variant ="h5"> Vehicle no. <CarSimple/> <Typography variant="h5"> KA00 XX 0000</Typography></Typography>
+                   <Typography variant ="h5"> at <Clock/> <Typography variant="h5"> 12:00</Typography></Typography>
+                  <Button variant="outline-primary"> Update Status</Button>
+                   </div>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Body>
+              
+              <div className="d-grid gap-2">
+                    <Recycle/><Typography> 472103847283</Typography>
+                   <Typography variant ="h4"> Plastic Type</Typography>
+                   <Typography variant ="h5"> Arriving at <MapPinLine/> <Typography variant="h5"> Station</Typography></Typography>
+                   <Typography variant ="h5"> Vehicle no. <CarSimple/> <Typography variant="h5"> KA00 XX 0000</Typography></Typography>
+                   <Typography variant ="h5"> at <Clock/> <Typography variant="h5"> 12:00</Typography></Typography>
+                  <Button variant="outline-primary"> Update Status</Button>
+                   </div>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Body>
+              
+              <div className="d-grid gap-2">
+                    <Recycle/><Typography> 472103847283</Typography>
+                   <Typography variant ="h4"> Plastic Type</Typography>
+                   <Typography variant ="h5"> Arriving at <MapPinLine/> <Typography variant="h5"> Station</Typography></Typography>
+                   <Typography variant ="h5"> Vehicle no. <CarSimple/> <Typography variant="h5"> KA00 XX 0000</Typography></Typography>
+                   <Typography variant ="h5"> at <Clock/> <Typography variant="h5"> 12:00</Typography></Typography>
+                  <Button variant="outline-primary"> Update Status</Button>
+                   </div>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Body>
+                
+              </Card.Body>
+            </Card>
+          </Stack>
+        </Container>
+      </Stack>
 
-                                    
-                                        </Button>
-
-
-                                        {/* Camera */}
-                                    </Form.Group>
-                                    {/* Summary */}
-                                    <Button variant="primary" type="submit" className="w-100">
-                                        Summary
-                                    </Button>
-                                </Form>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        </main>
-    );
+      <Navbar fixed="bottom" className="bg-light">
+        <Container>
+          <Button variant="outline-*" size="sm">
+            <List size={32} />
+          </Button>
+          <Button variant="outline-*" size="sm">
+            <House size={32} />
+          </Button>
+          <Button variant="outline-*" size="sm">
+            <User size={32} />
+          </Button>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
+
+{
+  /* <Container>
+            <Link href="#"
+                tooltip="Create note link"
+                icon="far fa-sticky-note" />
+            <Link href="#"
+                tooltip="Add user link"
+               ><Plus/></Link>
+               
+            <Button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false"
+                tooltip="The big plus button!"
+                rotate={true}
+                ><Plus/></Button>
+                
+        </Container>
+
+  */
+}
