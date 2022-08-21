c0ab7a4c463c23db5f00742c78975496579266b9
Pranavbala Ganesh <pranavb.ganesh@gmail.com>
2022-08-21 09:32:04 +0530

    basic profile page

--- src/pages/Mobile/RecyclerInput.js
+++ src/pages/Mobile/RecyclerInput.js
@@ -1,242 +1,213 @@
 import React, { useRef, useEffect, useState } from "react";
-
-import Webcam from "react-webcam";
-import { Link } from "react-router-dom";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-
 import {
-  faFacebookF,
-  faGithub,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
-import {
-  Col,
-  Row,
-  Card,
-  Button,
-  FormCheck,
+  Navbar,
   Container,
+  Button,
+  Image,
+  Row,
+  Col,
+  Stack,
+  Figure,
+  Form,
+  Card,
+  InputGroup,
+  ListGroup,
+  Modal,
 } from "react-bootstrap";
-
 import { Routes } from "../../routes";
-import BgImage from "../../assets/img/illustrations/signin.svg";
+import { Typography, Color, CardBody } from "@material-tailwind/react";
 import {
-  faCameraRetro,
-  faWeightHanging,
-  faAngleLeft,
-  faEnvelope,
-  faUnlockAlt,
-} from "@fortawesome/free-solid-svg-icons";
+  Scales,
+  CaretDown,
+  Camera,
+  List,
+  Recycle,
+  CarSimple,
+  House,
+  User,
+  Plus,
+  MapPinLine,
+  Clock,
+} from "phosphor-react";
+import { Link } from "react-floating-action-button";
 
 export default () => {
-  var fileInput = document.getElementById('weight-collector');
-  var fileDisplayArea = document.getElementById('fileDisplayArea');
+  const [show, setShow] = useState(false);
 
-  window.onload = function() {
-    var fileInput = document.getElementById('weight-collector');
-    var fileDisplayArea = document.getElementById('fileDisplayArea');
-
-    fileInput.addEventListener('change', function(e) {
-      // Put the rest of the demo code here.
-      var file = fileInput.files[0];
-      var imagetype = /image.*/;
-      if (file.type.match(imagetype)) {
-        var reader = new FileReader();
-        reader.onload = function(e){
-          fileDisplayArea.innerHTML = "";
-          var img = new Image();
-          img.src = reader.result;
-          fileDisplayArea.appendChild(img);
-        }
-        reader.readAsDataURL(file);
-    }else{
-      fileDisplayArea.innerHTML = "File not supported!";
-    }
-    });
-}
-
-  
-
-
-   
-
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <main>
-      <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+    <>
+      <Navbar variant="light">
         <Container>
-          <p className="text-center">
-            <Card.Link
-              as={Link}
-              to={Routes.DashboardOverview.path}
-              className="text-gray-700"
-            >
-              Back to homepage
-            </Card.Link>
-          </p>
-          <Row
-            className="justify-content-center form-bg-image"
-            style={{ backgroundImage: `url(${BgImage})` }}
-          >
-            <Col
-              xs={12}
-              className="d-flex align-items-center justify-content-center"
-            >
-              <div className="mb-4 mb-lg-0 bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
-                <div className="text-center text-md-center mb-4 mt-md-0">
-                  {/* Header */}
-                  <h3 className="mb-0">Recyler Input</h3>
-                </div>
-                <Form className="mt-4">
-                  <div className="mt-3 mb-4 text-center"></div>
-                  <div className=" justify-content-center my-4">
-                    {/* Weight */}
-                    <Form.Group id="weight" className="mb-4">
-                      <Form.Label>Weight of Collected waste</Form.Label>
-                      <InputGroup>
-                        <InputGroup.Text>
-                  
-                        </InputGroup.Text>
-
-                        <Form.Control
-                          autoFocus
-                          required
-                          type="weight"
-                          placeholder="100"
-                        />
-                      </InputGroup>
-                       {/* Camera */}
-                    </Form.Group>
-                    <Form.Group id="weight" className="mb-4">
-                      <Form.Label>Picture of Weight</Form.Label>
-                      <input
-                        type="file"
-                        id="weight-collector"
-                        name="weight-collector"
-                        accept="image/*"
-                        capture="environment"
-                      ></input>
-                       <div id="fileDisplayArea"></div> 
-              
-
-
-                     
-                    </Form.Group>
-                    {/* <div classname="camera">
-                     <video ref={videoref}></video>
-                    <Button
-                      variant="outline-light"
-                      className="btn-icon-only btn-pil text-dark"
-                    >
-                      SNAP!
-                   
-                    </Button>
-                    </div>
-                    <div classname={'result' + (hasPhoto ? 'hasPhoto' : '')}>
-                      <canvas ref={photoref}>
-                        <button>
-                          CLOSE!
-                        </button>
-                      </canvas>
-                    </div> */}
-                  </div>
-
-                  {/* Type of Plastic */}
-                  <Form.Group id="typeOfPlastic" className="mb-4">
-                    <Form.Label>Type of Plastic </Form.Label>
-                    <InputGroup>
-                      <DropdownButton
-                        variant="outline-secondary"
-                        title="Dropdown"
-                        id="input-group-dropdown-2"
-                        align="end"
-                      >
-                        <Dropdown.Item href="#">Pla-Plastic</Dropdown.Item>
-                        <Dropdown.Item href="#">Pla-Plastic</Dropdown.Item>
-                        <Dropdown.Item href="#">Pla-Plastic</Dropdown.Item>
-                      </DropdownButton>
-                      <Form.Control aria-label="Text input with dropdown button" />
-                    </InputGroup>
-                  </Form.Group>
-                  {/* Recycling Facility */}
-                  <Form.Group id="typeOfPlastic" className="mb-4">
-                    <Form.Label>Type of Plastic </Form.Label>
-                    <InputGroup>
-                      <DropdownButton
-                        variant="outline-secondary"
-                        title="Dropdown"
-                        id="input-group-dropdown-2"
-                        align="end"
-                      >
-                        <Dropdown.Item href="#">Facility 1</Dropdown.Item>
-                        <Dropdown.Item href="#">Facility 2</Dropdown.Item>
-                        <Dropdown.Item href="#">Facility 3</Dropdown.Item>
-                      </DropdownButton>
-                      <Form.Control aria-label="Text input with dropdown button" />
-                    </InputGroup>
-                  </Form.Group>
-                  {/* Images of waste collected */}
-                  <div class="ml-5">
-                    <Button class="mx-auto" s>
-                      <input
-                        type="file"
-                        id="weight-collector"
-                        name="weight-collector"
-                        accept="image/*"
-                        capture="environment"
-                      ></input>
-
-                    
-                    </Button>
-
-                    <Button>
-                      <input
-                        type="file"x
-                        id="weight-collector"
-                        name="weight-collector"
-                        accept="image/*"
-                        capture="environment"
-                      ></input>
-
-                   
-                    </Button>
-                    <Button>
-                      <input
-                        type="file"
-                        id="weight-collector"
-                        name="weight-collector"
-                        accept="image/*"
-                        capture="environment"
-                      ></input>
-
-                  
-                    </Button>
-                  </div>
-                  {/* Vehicle No. */}
-                  <Form.Group id="vehivle" className="mb-4">
-                    <Form.Label>Vehicle no.</Form.Label>
-                    <InputGroup>
-                      <Form.Control
-                        autoFocus
-                        required
-                        type="weight"
-                        placeholder="KAXX 0001"
-                      />
-                    </InputGroup>
-                  </Form.Group>
-                  {/* Summary */}
-                  <Button variant="primary" type="submit" className="w-100">
-                    Summary
-                  </Button>
-                </Form>
-              </div>
-            </Col>
-          </Row>
+          <Button variant="outline-*">
+            <List size={24} />
+          </Button>
         </Container>
-      </section>
-    </main>
+        <Container>
+          <Button variant="info" size="sm">
+            Contact Us
+          </Button>
+          <Button variant="outline-*" size="sm">
+            <img
+              src="https://picsum.photos/40/40"
+              width="20"
+              height="20"
+              class="rounded-circle"
+            ></img>
+          </Button>
+        </Container>
+      </Navbar>
+      <Container className="mb-3">
+        <Typography variant="h1" color="textPrimary">
+          Profile
+        </Typography>
+        <Stack direction="horizontal" spacing={4} className="mb-3">
+          <img
+            src="https://picsum.photos/40/40"
+            width="150"
+            height="150"
+            class="rounded-circle"
+          ></img>
+          <Container>
+            <Typography variant="h2" color="textPrimary">
+              {" "}
+              John Doe
+            </Typography>
+            <Typography variant="lead" color="textPrimary">
+              Collector
+            </Typography>
+          </Container>
+        </Stack>
+        <Stack direction="vertical" gap={3} className="mb-3">
+          <Card>
+            <Card.Body>
+              <Typography variant="h3" color="textPrimary">
+                {" "}
+                Registered Address
+              </Typography>
+              <Typography variant="paragraph" color="textPrimary">
+                4, 30th Main Rd, opp. Kempegowda Institute Of Medical Science,
+                Kaveri Nagar, Banagirinagara, Banashankari 3rd Stage,
+                Banashankari, Bengaluru, Karnataka 560070
+              </Typography>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Stack direction="horizontal" spacing={2}>
+                <Button variant="outline-*" onClick={handleShow}>
+                  <img
+                    src="https://picsum.photos/40/40"
+                    width="50"
+                    height="50"
+                    class="rounded-circle"
+                  ></img>
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Image of Facility</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <img
+                      src="https://picsum.photos/40/40"
+                      width="500"
+                      height="500"
+                    ></img>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                      Save Changes
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+
+                <Container>
+                  <Typography variant="h6" color="textPrimary">
+                    {" "}
+                    Image of Facility
+                  </Typography>
+                  <Typography variant="paragraph" color="textPrimary">
+                    Last Updated:12/07/2022 : 15:30
+                  </Typography>
+                </Container>
+              </Stack>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Typography variant="h3" color="textPrimary">
+                {" "}
+                GST Number{" "}
+              </Typography>
+              <Typography variant="lead">22AAAAA0000A1Z5</Typography>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Stack gap={2}>
+                <Typography variant="h3" color="textPrimary">
+                  {" "}
+                  Waste Categories{" "}
+                </Typography>
+                <ListGroup>
+                  <ListGroup.Item>this is 1</ListGroup.Item>
+                  <ListGroup.Item>this is 2</ListGroup.Item>
+                  <ListGroup.Item>this is 3</ListGroup.Item>
+                  <ListGroup.Item>this is 4</ListGroup.Item>
+                  <ListGroup.Item>this is 5</ListGroup.Item>
+                </ListGroup>
+              </Stack>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Typography variant="h3" color="textPrimary">
+                {" "}
+                Capacity{" "}
+              </Typography>
+              <ListGroup.Item>
+                <Typography variant="lead">100000 kgs </Typography>
+              </ListGroup.Item>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Typography variant="h3" color="textPrimary">
+                {" "}
+                Workers Strength{" "}
+              </Typography>
+              <ListGroup.Item>
+                <Typography variant="lead">100 - 150 people</Typography>
+              </ListGroup.Item>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <div class="mb-4"></div>
+            </Card.Body>
+          </Card>
+        </Stack>
+      </Container>
+
+      <Navbar fixed="bottom" className="bg-light">
+        <Container>
+          <Button variant="outline-*" size="sm">
+            <List size={32} />
+          </Button>
+          <Button variant="outline-*" size="sm">
+            <House size={32} />
+          </Button>
+          <Button variant="outline-*" size="sm">
+            <User size={32} />
+          </Button>
+        </Container>
+      </Navbar>
+    </>
   );
 };
